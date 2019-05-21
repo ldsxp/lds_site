ecfa3ef2f8a069c37ee619a83378f9de057dd271
ldsxp <85176878@qq.com>
2019-05-22 00:12:59 +0800

    + 添加访问统计（Middleware）

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,4 +1,7 @@
-from django.db.models import Q
+from datetime import date
+
+from django.core.cache import cache
+from django.db.models import Q, F
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
@@ -82,6 +85,35 @@ class PostDetailView(CommonViewMixin, DetailView):
     #         'comment_list': Comment.get_by_target(self.request.path),
     #     })
     #     return context
+
+    def get(self, request, *args, **kwargs):
+        response = super().get(request, *args, **kwargs)
+        self.handle_visited()
+        # 调试用，查看 SQL 语句，我们有很多方法调试，这里打印不是一个好办法
+        # from django.db import connection
+        # print(connection.queries)
+        return response
+
+    def handle_visited(self):
+        increase_pv = False
+        increase_uv = False
+        uid = self.request.uid
+        pv_key = 'pv:%s:%s' % (uid, self.request.path)
+        if not cache.get(pv_key):
+            increase_pv = True
+            cache.set(pv_key, 1, 1 * 60)  # 1分钟有效
+
+        uv_key = 'uv:%s:%s:%s' % (uid, str(date.today()), self.request.path)
+        if not cache.get(uv_key):
+            increase_uv = True
+            cache.set(uv_key, 1, 24 * 60 * 60)  # 24小时有效
+
+        if increase_pv and increase_uv:
+            Post.objects.filter(pk=self.object.id).update(pv=F('pv') + 1, uv=F('uv') + 1)
+        elif increase_pv:
+            Post.objects.filter(pk=self.object.id).update(pv=F('pv') + 1)
+        elif increase_uv:
+            Post.objects.filter(pk=self.object.id).update(uv=F('uv') + 1)
 
 
 class SearchView(IndexView):
